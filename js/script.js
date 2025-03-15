823fb389c18187f151a34fdab08f0c47e283b9f8
hxezu <hailey_apple@icloud.com>
2025-03-15 21:36:32 +0900

    feat: Create .con02 Section ; MyWork

--- js/script.js
+++ js/script.js
@@ -34,15 +34,17 @@ $(function(){
     })
 })
 
-//con01 gsap 애니매이션
+
 $(function(){
+
+    //con01 gsap 애니매이션
     gsap.timeline({
         scrollTrigger:{
             trigger :'.con01',
             start:'0% 80%',
             end:'100% 100%',
             scrub : 1,
-            markers:true
+            //markers:true
         }
     })
     .to('.wrap',{'backgroundColor':'#fff','color':'#000','ease':'none','duration' : '5'},0)
@@ -50,4 +52,48 @@ $(function(){
     .to('.scroll', {'opacity':'0','ease' : 'none','duration' : '5'},0)
     .fromTo('.videoWrap video',{'clip-path':'inset(60% 60% 60% 60% round 50%)'}
         ,{'clip-path':'inset(0% 0% 0% 0% round 0%)',ease:'none', duration:10},0)
+
+
+    //con02 title 텍스트 애니매이션
+    gsap.timeline({
+        scrollTrigger:{
+            trigger:'.con02',
+            start : '0%, 100%',
+            end: '0% 20%',
+            scrub:1,
+            //markers: true
+        }
+    })
+    .fromTo('.con02 .title .a', {x:'-100%'}, {x:'0%', ease:'none', duration:5},0)
+    .fromTo('.con02 .title .b', {x:'100%'}, {x:'0%', ease:'none', duration:5},0)
+
+    //con02 애니메이션
+    gsap.timeline({
+        scrollTrigger:{
+            trigger:'.workList',
+            start:'0% 100%',
+            end: '0% 100%',
+            scrub:1,            //되감기가 될수 있도록
+            //markers:true
+        }
+    })
+    //worklist 나타나면 배경색 검정
+    .to('.wrap', {backgroundColor:"#000", color:"#fff", ease:'none', duration:5},0)
+    //title 배경에 고정
+    .to('.con02 .title', {position:'fixed', ease:'none', left:'0', top:'0', width:'100%', duration:5},0)
+    //worklist 에 margin-top 을 적용해서 부드럽게 올라오도록
+    .fromTo('.workList', {margin:'0 auto'}, {margin:'100vh auto 0', position:'relative', zIndex:'10', duration:1},0)
+
+    //con02 가 끝날 때
+    gsap.timeline({
+        scrollTrigger:{
+            trigger:'.workList',
+            start:'100% 50%',
+            end: '100% 0%',
+            scrub:1,         
+            //markers:true
+        }
+    })
+    .to('.con02 .title .a', {x:'-100%', ease:'none', duration:5},0)
+    .to('.con02 .title .b', {x:'100%', ease:'none', duration:5},0)
 })
